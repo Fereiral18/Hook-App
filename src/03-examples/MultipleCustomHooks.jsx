3db417515d1a5962d33c89a4ce74818a419fdc3b
Luis Fereira <luis@spartandevs.com>
2023-01-29 01:34:26 -0400

    useFetch custom hook

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -1,14 +1,26 @@
-import { useFetch } from "../hooks/useFetch"
-
+import { useFetch } from "../hooks/useFetch";
 
 export const MultipleCustomHooks = () => {
-  const {data, isLoading, hasError} = useFetch('https://api.breakingbadquotes.xyz/v1/quotes/1')
-  
-  console.log({data, isLoading, hasError})
-    return (
+  const { data, isLoading, hasError } = useFetch(
+    "https://api.breakingbadquotes.xyz/v1/quotes/1"
+  );
+
+const {author, quote} = !!data && data[0]
+
+  return (
     <>
-        <h1>BreakingBad Quotes</h1>
-        <hr />
+      <h1>BreakingBad Quotes</h1>
+      <hr />
+
+      {isLoading 
+           ? (
+        <div className="alert alert-info text-center">Loading...</div>
+      ) : (
+        <blockquote className="blockquote text-end">
+          <p className="mb-1">{quote}</p>
+          <footer className="blockquote-footer">{author}</footer>
+        </blockquote>
+      )}
     </>
-  )
-}
+  );
+};
