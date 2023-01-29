e4439d2184a15db8c48a35c29413ebe79ce00d18
Luis Fereira <luis@spartandevs.com>
2023-01-29 00:14:59 -0400

    useFetch custom hook

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ForWithCustomHook } from "./02-useEffect/ForWithCustomHook";
+import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
+// import { ForWithCustomHook } from "./02-useEffect/ForWithCustomHook";
 
 // import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
 // import { CounterApp } from './01-useState/CounterApp'
 // import { HooksApp } from './HooksApp'
-import { SimpleForm } from "./02-useEffect/SimpleForm";
+// import { SimpleForm } from "./02-useEffect/SimpleForm";
 
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-    <ForWithCustomHook />
+    <MultipleCustomHooks />
   /* </React.StrictMode> */
 );
