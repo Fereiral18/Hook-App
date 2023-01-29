822902573dd8b533db3efc543ad713d7b4512914
Luis Fereira <luis@spartandevs.com>
2023-01-29 02:42:54 -0400

    optimization of components

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -1,26 +1,30 @@
-import { useFetch } from "../hooks/useFetch";
+import { useCounter, useFetch } from "../hooks";
+import { LoadingQuoter, Quote } from "./";
+
+
 
 export const MultipleCustomHooks = () => {
+  const { counter, increment, decrement } = useCounter(1);
+
   const { data, isLoading, hasError } = useFetch(
-    "https://api.breakingbadquotes.xyz/v1/quotes/1"
+    `https://api.breakingbadquotes.xyz/v1/quotes/${counter}`
   );
 
-const {author, quote} = !!data && data[0]
+  const { author, quote } = !!data && data[0];
 
   return (
     <>
       <h1>BreakingBad Quotes</h1>
       <hr />
 
-      {isLoading 
-           ? (
-        <div className="alert alert-info text-center">Loading...</div>
-      ) : (
-        <blockquote className="blockquote text-end">
-          <p className="mb-1">{quote}</p>
-          <footer className="blockquote-footer">{author}</footer>
-        </blockquote>
-      )}
+      {isLoading ? <LoadingQuoter /> : <Quote author={ author } quote = {quote} />}
+
+      <button onClick={() => increment()} className="btn btn-primary">
+        Next quote
+      </button>
+      <button onClick={() => decrement()} className="btn btn-primary mt-2">
+        Back quote
+      </button>
     </>
   );
 };
