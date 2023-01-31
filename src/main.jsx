312eb4d3e5b2238af928c6b0a64206e702c513d0
Luis Fereira <luis@spartandevs.com>
2023-01-30 20:17:14 -0400

    practice of useRef

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
+import { FocusScreen } from "./04-useRef/FocusScreen";
+// import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
 // import { ForWithCustomHook } from "./02-useEffect/ForWithCustomHook";
 
 // import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
@@ -12,6 +13,6 @@ import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-    <MultipleCustomHooks />
+    <FocusScreen />
   /* </React.StrictMode> */
 );
