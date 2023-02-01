af2cd41ede7a909416fabc1f02c959fc33882137
Luis Fereira <luis@spartandevs.com>
2023-02-01 00:06:57 -0400

    useMemo hook

--- src/06-memos/MemoHook.jsx
+++ src/06-memos/MemoHook.jsx
@@ -1,16 +1,29 @@
-import { useState } from "react"
+import { useMemo, useState } from "react"
 import { useCounter } from "../hooks"
-import { Smal } from "./Smal"
 
-export const Memorize = () => {
+
+const heavyStuff = ( iterationNumber = 100 ) =>{
+    for(let i = 0; i< iterationNumber; i++){
+      console.log('ahi vamos...')
+    }
+    return `${ iterationNumber } iteraciones realizadas`
+}
+
+
+
+export const MemoHook = () => {
   
-    const { counter, increment} = useCounter(10)
+    const { counter, increment} = useCounter(2000)
     const [show, setShow] = useState(true)
   
+   const memorizeValue = useMemo(() => heavyStuff(counter), [counter])
+
     return (
     <>
-    <h1>counter: <Smal value={ counter }/> </h1>
+    <h1>counter: <small>{counter}</small> </h1>
     <hr />
+
+    <h4>{memorizeValue}</h4>
 
     <button className="btn btn-primary"
     onClick={()=> increment()}
--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Memorize } from "./06-memos/memorize";
 
-// import { Layout } from "./05-useLayaoutEffect/Layaout";
-// import { FocusScreen } from "./04-useRef/FocusScreen";
-// import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
-// import { ForWithCustomHook } from "./02-useEffect/ForWithCustomHook";
 
 // import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
 // import { CounterApp } from './01-useState/CounterApp'
 // import { HooksApp } from './HooksApp'
+// import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
 // import { SimpleForm } from "./02-useEffect/SimpleForm";
+// import { ForWithCustomHook } from "./02-useEffect/ForWithCustomHook";
+// import { FocusScreen } from "./04-useRef/FocusScreen";
+// import { Layout } from "./05-useLayaoutEffect/Layaout";
+// import { Memorize } from "./06-memos/memorize";
+import { MemoHook } from "./06-memos/MemoHook";
 
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-    <Memorize/>
+    <MemoHook/>
   /* </React.StrictMode> */
 );
