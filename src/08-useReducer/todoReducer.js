26be8262ab6ef398e7050da955e442528b95642c
Luis Fereira <luis@spartandevs.com>
2023-02-06 14:02:01 -0400

    implementation of dispatch, adds elements a input

--- src/08-useReducer/todoReducer.js
+++ src/08-useReducer/todoReducer.js
@@ -1,11 +1,11 @@
 
 
-export const todoReducer = () =>{
+export const todoReducer = ( initialState=[], action ) =>{
 
     switch (action.type) {
-        case 'ABC':
+        case 'Add Todo':
             
-           throw new Error('Action.type = ABC no esta implementada')
+           return [...initialState, action.payload]
     
         default:
             return initialState;
